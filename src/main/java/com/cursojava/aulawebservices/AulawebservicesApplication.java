5efebc0bc73214ba8b89edacca610b673893d5a5
Marcelo Ishii <marcelokoiti2014@gmail.com>
2021-03-02 17:43:16 -0300

    Project created

---
+++ src/main/java/com/cursojava/aulawebservices/AulawebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.cursojava.aulawebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulawebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulawebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursojava/aulawebservices/AulawebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojava.aulawebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulawebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
