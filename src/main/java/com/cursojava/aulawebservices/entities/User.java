ce18e1f2ef124a6dbda6616681b47a6146b4bd6e
Marcelo Ishii <marcelokoiti2014@gmail.com>
2021-03-03 12:19:29 -0300

    H2 database, test profile, JPA

--- src/main/java/com/cursojava/aulawebservices/entities/User.java
+++ src/main/java/com/cursojava/aulawebservices/entities/User.java
@@ -2,13 +2,25 @@ package com.cursojava.aulawebservices.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 // Interface Serializable - transforma objeto em bytes para trafegar na rede,
 // para objeto ser gravado em arquivo, etc..
 
+//Aula302 - Incluir annotations para JPA converter objetos em relacional
+
+//Aula302
+@Entity 
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	
+
+	//Aula302 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
