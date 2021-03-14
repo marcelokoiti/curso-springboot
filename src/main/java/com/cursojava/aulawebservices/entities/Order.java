baaae6f4f4e7440b936a1ed378a05b810b4bcae7
Marcelo Ishii <marcelokoiti2014@gmail.com>
2021-03-14 10:31:31 -0300

    Payment, one-to-one association

--- src/main/java/com/cursojava/aulawebservices/entities/Order.java
+++ src/main/java/com/cursojava/aulawebservices/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,11 +13,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.cursojava.aulawebservices.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 // Dando o nome da tabela order para tb_order, pois esta dando conflito com palavra order
@@ -55,6 +56,10 @@ public class Order implements Serializable {
 	//No OrderItem nao temos o atributo Order de forma direta
 	//Temos o id que tem relacao com o Order
 	private Set<OrderItem> items = new HashSet<>(); 
+
+	//Mapeamento OneToOne com mesmo id (Order e Payment)-parametro cascade
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 	}
@@ -101,6 +106,14 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	public Set<OrderItem> getItems() {
