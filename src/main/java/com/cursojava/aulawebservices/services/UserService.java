be6be1cf4749a2772a3427d969fa59398c5244cd
Marcelo Ishii <marcelokoiti2014@gmail.com>
2021-03-16 15:20:37 -0300

    Exception handling - delete

--- src/main/java/com/cursojava/aulawebservices/services/UserService.java
+++ src/main/java/com/cursojava/aulawebservices/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.cursojava.aulawebservices.entities.User;
 import com.cursojava.aulawebservices.repositories.UserRepository;
+import com.cursojava.aulawebservices.services.exceptions.DatabaseException;
 import com.cursojava.aulawebservices.services.exceptions.ResourceNotFoundException;
 
 //Camada SERVICE
@@ -44,7 +47,13 @@ public class UserService {
 	}
 	
 	public void delete (Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
@@ -61,5 +70,4 @@ public class UserService {
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
 	}
-	
 }
